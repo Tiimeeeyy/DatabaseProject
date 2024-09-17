a249e4b3c7ac8f19f08612ebd3a29d977900be86
taha <t.koc@student.maastrichtuniversity.nl>
2024-09-17 23:21:18 +0200

    Added OrderHistory and also added a new column for the Drinks table 'is_special' that determines if the drink is a special or event drink which is useful for the next functionality, 'favourite_item', so that if a customer purchases a lot of sodas it doesn't appear as the customer's favourite item unlike a milkshake or a meal.

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import sqlite3
 import threading
 import json
 from datetime import datetime
+from dis import disco
 
 
 def handle_client(client_socket):
@@ -27,7 +28,19 @@ def handle_client(client_socket):
 		elif action == 'get_menu':
 			menu = get_menu(cursor)
 			client_socket.send(json.dumps(menu).encode('utf-8'))
-
+		elif action == 'get_history':
+			customer_id = request_data['customer_id']
+			order_history = get_history(cursor, customer_id)
+			client_socket.send(json.dumps(order_history).encode('utf-8'))
+		elif action == 'get_favourite_item':
+			customer_id = request_data['customer_id']
+			favourite_items = get_favourite_item(cursor, customer_id)
+			if favourite_items:
+				client_socket.send(json.dumps({favourite_items}).encode('utf-8'))
+			else:
+				client_socket.send(json.dumps({'message': 'No purchases found to determine favorite item.'}).encode('utf-8'))
+		else:
+			client_socket.send(json.dumps({'error': 'Unknown action/procedure'}).encode('utf-8'))
 
 	conn.close()
 	client_socket.close()
@@ -81,6 +94,133 @@ def get_menu(cursor):
 		'drinks': drinks,
 		'deserts': deserts
 	}
+
+def get_history(cursor, customer_id, discount_applied):
+	cursor.execute('''
+	SELECT id, order_date, status, discount_applied, total_price
+	FROM Orders
+	WHERE customer_id = ?
+	ORDER BY order_date DESC
+''', (customer_id,))
+	orders = cursor.fetchall()
+
+	order_history = []
+	for order in orders:
+		order_id, order_date, status, discount_applies, total_price = order
+		cursor.execute('''
+			SELECT item_type, item_id, quantity
+			FROM OrderItems
+			WHERE order_id = ?
+		''', (order_id,))
+		items = cursor.fetchall()
+
+		order_items = []
+		for item in items:
+			item_type, item_id, quantity = item
+			if item_type == 'pizza':
+				cursor.execute('SELECT name, price FROM Pizzas WHERE id = ?', (item_id))
+			elif item_type == 'drink':
+				cursor.execute('SELECT name, price FROM Drinks WHERE id = ?', (item_id,))
+			elif item_type == 'dessert':
+				cursor.execute('SELECT name, price FROM Desserts WHERE id = ?', (item_id,))
+			item_info = cursor.fetchone()
+			if item_info:
+				name, price = item_info
+				order_items.append({
+					'item_type': item_type,
+					'item_id': item_id,
+					'name': name,
+					'quantity': quantity,
+					'price': price
+				})
+
+		order_history.append(
+			{
+				'order_id': order_id,
+				'orer_date': order_date,
+				'status': status,
+				'discount_applied': discount_applied,
+				'total_price': total_price,
+				'items': order_items
+			})
+
+	return order_history
+
+
+def get_favourite_item(cursor, customer_id):
+	query = """
+	WITH FilteredOrderItems AS(
+		SELECT
+			oi.customer_id,
+			oi.item_type,
+			oi.item_id
+			oi.quantity
+		FROM
+			OrderItems oi
+		LEFT JOIN
+			Drinks d ON oi.item_type = 'drink' AND oi.item_id = d.id
+		WHERE
+			NOT (oi.item_type = 'drink' AND d.is_special = 0)
+	),
+	AggregatedItems AS(
+		SELECT
+			customer_id,
+			item_type,
+			item_id,
+			SUM(quantity) AS total_quantity
+		FROM
+			FilteredOrderItems
+		WHERE
+			customer_id = ?
+		GROUP BY
+			customer_id, item_type, item_id
+	),
+	MaxQuantities AS (
+		SELECT
+			customer_id
+			MAX(total_quantity) AS max_quantity
+		FROM
+			AggregatedItems
+		GROUP BY
+			customer_id
+	)
+	SELECT
+		ai.customer_id,
+		ai.item_type,
+		ai.item_id,
+		ai.total_quantity,
+		CASE
+			WHEN ai.item_type = 'pizza' THEN p.name
+			WHEN ai.item_type = 'drink' THEN d.name
+			WHEN ai.item_type = 'dessert' THEN ds.name
+		END AS item_name
+	FROM AggregatedItems ai
+	JOIN 
+		MaxQuantities mq on ai.customer_id = mq.customer_id AND ai.total_quantity = mq.max_quantity
+    LEFT JOIN 
+        Pizzas p ON ai.item_type = 'pizza' AND ai.item_id = p.id
+    LEFT JOIN 
+       	Drinks d ON ai.item_type = 'drink' AND ai.item_id = d.id
+    LEFT JOIN 
+        Desserts ds ON ai.item_type = 'dessert' AND ai.item_id = ds.id
+    WHERE 
+        ai.customer_id = ?
+	"""
+
+	cursor.execute(query, (customer_id, customer_id))
+	result = cursor.fetchall()
+
+	favourite_items = []
+	for row in result:
+		customer_id, item_type, item_id, total_quantity, item_name = row
+		favourite_items.append({
+			'item_type': item_type,
+			'item_id': item_id,
+			'item_name': item_name,
+			'total_quantity': total_quantity
+		})
+
+	return favourite_items
 
 def server():
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
